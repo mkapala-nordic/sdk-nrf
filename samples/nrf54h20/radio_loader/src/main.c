1c7bc781b83a9e534a731fd0974cf2f2092e143e
Mateusz Kapala <mateusz.kapala@nordicsemi.no>
2025-12-01 13:59:22 +0100

    [wip] samples: idle_relocated_tcm: Simplify configurations
    Changed DTS node selection in radio_loader.
    Simplified configurations needed to run from TCM.
    
    Jira: NCSDK-36457
    
    Signed-off-by: Mateusz Kapala <mateusz.kapala@nordicsemi.no>

--- samples/nrf54h20/radio_loader/src/main.c
+++ samples/nrf54h20/radio_loader/src/main.c
@@ -6,29 +6,50 @@
 
 #include <zephyr/kernel.h>
 
-/* The loader uses devicetree chosen nodes to specify source
- * and destination memory regions:
- * - zephyr,loaded-fw-src: Source partition in NVM
- * - zephyr,loaded-fw-dst: Destination region in RAM
- */
+#define FIXED_PARTITION_ADDRESS(label)                                                             \
+	(DT_REG_ADDR(DT_NODELABEL(label)) +                                                        \
+	 DT_REG_ADDR(COND_CODE_1(DT_FIXED_SUBPARTITION_EXISTS(DT_NODELABEL(label)),                \
+			(DT_GPARENT(DT_PARENT(DT_NODELABEL(label)))),                              \
+			(DT_GPARENT(DT_NODELABEL(label))))))
 
-#define LOADED_FW_NVM_NODE				DT_CHOSEN(zephyr_loaded_fw_src)
-#define LOADED_FW_NVM_PARTITION_NODE			DT_PARENT(DT_PARENT(LOADED_FW_NVM_NODE))
-#define LOADED_FW_NVM_ADDR				(DT_REG_ADDR(LOADED_FW_NVM_NODE) + \
-							 DT_REG_ADDR(LOADED_FW_NVM_PARTITION_NODE))
-#define LOADED_FW_NVM_SIZE				DT_REG_SIZE(LOADED_FW_NVM_NODE)
+#define FIXED_PARTITION_SIZE(label) DT_REG_SIZE(DT_NODELABEL(label))
 
-#define LOADED_FW_RAM_NODE				DT_CHOSEN(zephyr_loaded_fw_dst)
-#define LOADED_FW_RAM_ADDR				DT_REG_ADDR(LOADED_FW_RAM_NODE)
-#define LOADED_FW_RAM_SIZE				DT_REG_SIZE(LOADED_FW_RAM_NODE)
+#ifdef CONFIG_USE_DT_CODE_PARTITION
+#define FLASH_LOAD_OFFSET DT_REG_ADDR(DT_CHOSEN(zephyr_code_partition))
+#elif defined(CONFIG_FLASH_LOAD_OFFSET)
+#define FLASH_LOAD_OFFSET CONFIG_FLASH_LOAD_OFFSET
+#endif
 
-/* Verify devicetree configuration at build time */
-BUILD_ASSERT(DT_NODE_EXISTS(DT_CHOSEN(zephyr_loaded_fw_src)),
-	     "Missing chosen node: zephyr,loaded-fw-src");
-BUILD_ASSERT(DT_NODE_EXISTS(DT_CHOSEN(zephyr_loaded_fw_dst)),
-	     "Missing chosen node: zephyr,loaded-fw-dst");
-BUILD_ASSERT(LOADED_FW_NVM_SIZE <= LOADED_FW_RAM_SIZE,
-	     "Firmware size exceeds available TCM RAM");
+#define PARTITION_IS_RUNNING_APP_PARTITION(label)                                                  \
+	(DT_REG_ADDR(DT_NODELABEL(label)) <= FLASH_LOAD_OFFSET &&                                  \
+	 DT_REG_ADDR(DT_NODELABEL(label)) + DT_REG_SIZE(DT_NODELABEL(label)) > FLASH_LOAD_OFFSET)
+
+/* Used to determine the running slot of the radio loader. */
+#define CPURAD_LOADER_PRIMARY_SLOT	cpurad_slot0_partition
+#define CPURAD_LOADER_SECONDARY_SLOT	cpurad_slot1_partition
+
+/* Used to determine the loaded firmware source address from NVM and size. */
+#define CPURAD_LOADED_FW_PRIMARY_SLOT	cpurad_slot2_partition
+#define CPURAD_LOADED_FW_SECONDARY_SLOT	cpurad_slot3_partition
+
+/* Used to determine the running slot of the radio core. */
+#define CPURAD_RUNNING_SLOT		cpurad_ram0
+#define CPURAD_RUNNING_SLOT_NODE	DT_NODELABEL(CPURAD_RUNNING_SLOT)
+#define CPURAD_RUNNING_SLOT_ADDR	DT_REG_ADDR(CPURAD_RUNNING_SLOT_NODE)
+#define CPURAD_RUNNING_SLOT_SIZE	DT_REG_SIZE(CPURAD_RUNNING_SLOT_NODE)
+
+BUILD_ASSERT(DT_NODE_EXISTS(DT_NODELABEL(CPURAD_LOADER_PRIMARY_SLOT)),
+			    "Missing nodelabel: cpurad_slot0_partition");
+BUILD_ASSERT(DT_NODE_EXISTS(DT_NODELABEL(CPURAD_LOADER_SECONDARY_SLOT)),
+			    "Missing nodelabel: cpurad_slot1_partition");
+
+BUILD_ASSERT(DT_NODE_EXISTS(DT_NODELABEL(CPURAD_LOADED_FW_PRIMARY_SLOT)),
+			    "Missing nodelabel: cpurad_slot2_partition");
+BUILD_ASSERT(DT_NODE_EXISTS(DT_NODELABEL(CPURAD_LOADED_FW_SECONDARY_SLOT)),
+			    "Missing nodelabel: cpurad_slot3_partition");
+
+BUILD_ASSERT(DT_NODE_EXISTS(DT_NODELABEL(CPURAD_RUNNING_SLOT)),
+			    "Missing nodelabel: cpurad_ram0");
 
 /**
  * @brief Copy firmware from MRAM to TCM and jump to it
@@ -43,11 +64,30 @@ BUILD_ASSERT(LOADED_FW_NVM_SIZE <= LOADED_FW_RAM_SIZE,
  */
 static int load_and_jump_to_firmware(void)
 {
+	/* Get the loaded firmware source address from NVM and size */
+	void *loaded_fw_nvm_addr = NULL;
+	size_t loaded_fw_nvm_size = 0;
+
+	/* Get the loaded firmware destination address from RAM and size */
+	void *loaded_fw_ram_addr = (void *)(CPURAD_RUNNING_SLOT_ADDR);
+	size_t loaded_fw_ram_size = (size_t)(CPURAD_RUNNING_SLOT_SIZE);
+
+	if (PARTITION_IS_RUNNING_APP_PARTITION(CPURAD_LOADER_PRIMARY_SLOT)) {
+		loaded_fw_nvm_addr = (void *)(FIXED_PARTITION_ADDRESS(CPURAD_LOADED_FW_PRIMARY_SLOT));
+		loaded_fw_nvm_size = (size_t)(FIXED_PARTITION_SIZE(CPURAD_LOADED_FW_PRIMARY_SLOT));
+	} else {
+		loaded_fw_nvm_addr = (void *)(FIXED_PARTITION_ADDRESS(CPURAD_LOADED_FW_SECONDARY_SLOT));
+		loaded_fw_nvm_size = (size_t)(FIXED_PARTITION_SIZE(CPURAD_LOADED_FW_SECONDARY_SLOT));
+	}
+
+	__ASSERT(loaded_fw_nvm_size <= loaded_fw_ram_size, "Loaded firmware NVM size exceeds available TCM RAM");
+	ARG_UNUSED(loaded_fw_ram_size);
+
 	/* Copy firmware from MRAM to TCM */
-	memcpy((void *)LOADED_FW_RAM_ADDR, (void *)LOADED_FW_NVM_ADDR, LOADED_FW_NVM_SIZE);
+	memcpy(loaded_fw_ram_addr, loaded_fw_nvm_addr, loaded_fw_nvm_size);
 
 	/* Extract reset handler from ARM Cortex-M vector table (entry 1) */
-	uint32_t *vector_table = (uint32_t *)(LOADED_FW_RAM_ADDR + CONFIG_ROM_START_OFFSET);
+	uint32_t *vector_table = (uint32_t *)((uint8_t *)loaded_fw_ram_addr + CONFIG_ROM_START_OFFSET);
 	typedef void reset_handler_t(void);
 	reset_handler_t *reset_handler = (reset_handler_t *)(vector_table[1]);
 
